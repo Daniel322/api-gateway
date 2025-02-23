f39a93527ba875d3607573ba139c8553a7974595
Daniel322 <kravchenkodanil122@gmail.com>
2025-02-24 00:23:26 +0400

    break nats adapter))

--- pkgs/brokers/nats/nats-adapter.go
+++ pkgs/brokers/nats/nats-adapter.go
@@ -12,27 +12,64 @@ var natsConnection *nats.Conn
 var methods map[string]any
 
 func Connect(url string, options nats.Options) error {
-	nc, err := nats.Connect(url)
+	nc, err := nats.Connect(url, nats.Name(options.User))
 	fmt.Println("succesfull connect to NATS!")
 
 	natsConnection = nc
 
-	natsConnection.Subscribe("$SRV.REGISTER", func(m *nats.Msg) {
+	nc.Subscribe("$SYS.> ", func(m *nats.Msg) {
+		fmt.Println(m.Subject)
 		var stringData string = string(m.Data)
-		fmt.Println(stringData, "\n")
+		fmt.Println(stringData)
 		var jsonData map[string]map[string]any
 		json.Unmarshal(m.Data, &jsonData)
-		fmt.Println(jsonData["info"]["endpoints"])
-		// for key, value := range jsonData {
-		// 	if reflect.TypeOf(value).Kind().String() == "map" {
-		// 		for infoKey, infoValue := range value {
-		// 			fmt.Println("key:", infoKey, "value:", infoValue)
-		// 		}
-		// 	} else {
-		// 		fmt.Println("key:", key, "value:", value)
-		// 	}
-		// }
+		fmt.Println(jsonData)
 	})
+
+	// natsConnection.Subscribe("$SYS.SERVER.*.STATSZ ", func(m *nats.Msg) {
+	// 	fmt.Println("STATS")
+	// 	var stringData string = string(m.Data)
+	// 	fmt.Println(stringData)
+	// 	var jsonData map[string]map[string]any
+	// 	json.Unmarshal(m.Data, &jsonData)
+	// 	fmt.Println(jsonData)
+	// })
+
+	// natsConnection.Subscribe("$SYS.REQ.SERVER.PING", func(m *nats.Msg) {
+	// 	fmt.Println("PING")
+	// 	var stringData string = string(m.Data)
+	// 	fmt.Println(stringData)
+	// 	var jsonData map[string]map[string]any
+	// 	json.Unmarshal(m.Data, &jsonData)
+	// 	fmt.Println(jsonData)
+	// })
+
+	// natsConnection.Subscribe("$SYS.SERVER.*.CLIENT.CONNECT", func(m *nats.Msg) {
+	// 	fmt.Println("connect \n")
+	// 	var stringData string = string(m.Data)
+	// 	fmt.Println(stringData, "\n")
+	// 	var jsonData map[string]map[string]any
+	// 	json.Unmarshal(m.Data, &jsonData)
+	// 	fmt.Println(jsonData)
+	// 	// for key, value := range jsonData {
+	// 	// 	if reflect.TypeOf(value).Kind().String() == "map" {
+	// 	// 		for infoKey, infoValue := range value {
+	// 	// 			fmt.Println("key:", infoKey, "value:", infoValue)
+	// 	// 		}
+	// 	// 	} else {
+	// 	// 		fmt.Println("key:", key, "value:", value)
+	// 	// 	}
+	// 	// }
+	// })
+
+	// natsConnection.Subscribe("$SYS.SERVER.*.CLIENT.DISCONNECT", func(message *nats.Msg) {
+	// 	fmt.Println("disconnect \n")
+	// 	var stringData string = string(message.Data)
+	// 	fmt.Println(stringData, "\n")
+	// 	var jsonData map[string]map[string]any
+	// 	json.Unmarshal(message.Data, &jsonData)
+	// 	fmt.Println(jsonData)
+	// })
 
 	return err
 }
