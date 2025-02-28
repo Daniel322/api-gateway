3d5473917502f1e9cdca23f57a2fafbb46ea55f0
Daniel322 <kravchenkodanil122@gmail.com>
2025-02-28 23:38:29 +0400

    add interval to srv info listener inbox, also start to work on methods slice: add methods on srv register

--- pkgs/brokers/nats/nats-adapter.go
+++ pkgs/brokers/nats/nats-adapter.go
@@ -1,15 +1,22 @@
 package natsadapter
 
 import (
+	"encoding/json"
 	"fmt"
 	"time"
+	"websocket-gateway/pkgs/utils"
 
 	nats "github.com/nats-io/nats.go"
 )
 
+type Method struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+}
+
 var natsConnection *nats.Conn
 
-var methods map[string]any
+var methods []Method
 
 func Connect(url string, options nats.Options) error {
 	nc, err := nats.Connect(url, nats.UserInfo(options.User, options.Password), nats.PermissionErrOnSubscribe(true))
@@ -19,45 +26,44 @@ func Connect(url string, options nats.Options) error {
 	fmt.Println("succesfull connect to NATS!")
 
 	natsConnection = nc
-
-	_, err = natsConnection.Subscribe("$SRV.>", func(m *nats.Msg) {
+	fmt.Println(methods)
+	_, err = natsConnection.Subscribe("$SRV.REGISTER", func(m *nats.Msg) {
 		fmt.Println(m.Subject)
 		var stringData string = string(m.Data)
 		fmt.Println(m.Subject, stringData)
-		// var jsonData map[string]map[string]any
-		// json.Unmarshal(m.Data, &jsonData)
-		// fmt.Println(m.Subject, "jsonData", jsonData)
+		var jsonData map[string]map[string]any
+		json.Unmarshal(m.Data, &jsonData)
+		methods = append(methods, Method{Id: jsonData["info"]["id"].(string), Name: jsonData["info"]["name"].(string)})
+		fmt.Println(methods)
 	})
 
 	if err != nil {
 		fmt.Println("subscribe on srv info error", err)
 	}
 
-	srvInfoInbox := natsConnection.NewInbox()
+	utils.SetInterval(func() {
+		srvInfoInbox := natsConnection.NewInbox()
 
-	srvInfoSubsribe, err := natsConnection.SubscribeSync(srvInfoInbox)
-	if err != nil {
-		fmt.Println("create sync sub error", err)
-		return err
-	}
-	defer srvInfoSubsribe.Unsubscribe() // Гарантированная отписка
-
-	err = natsConnection.PublishRequest("$SRV.INFO", srvInfoInbox, []byte(""))
-	if err != nil {
-		fmt.Println("publish request error", err)
-		return err
-	}
-
-	for {
-		msg, err := srvInfoSubsribe.NextMsg(10 * time.Millisecond)
+		srvInfoSubsribe, err := natsConnection.SubscribeSync(srvInfoInbox)
 		if err != nil {
-			fmt.Println("read srv info messages error", err)
-			break
+			fmt.Println("create sync sub error", err)
 		}
-		fmt.Println(string(msg.Data))
-	}
+		defer srvInfoSubsribe.Unsubscribe()
 
-	// TODO: make such inbox with SRV.PING
+		err = natsConnection.PublishRequest("$SRV.INFO", srvInfoInbox, []byte(""))
+		if err != nil {
+			fmt.Println("publish request error", err)
+		}
+
+		for {
+			msg, err := srvInfoSubsribe.NextMsg(10 * time.Millisecond)
+			if err != nil {
+				fmt.Println("read srv info messages error", err)
+				break
+			}
+			fmt.Println(string(msg.Data))
+		}
+	}, 60*time.Second)
 
 	return err
 }
