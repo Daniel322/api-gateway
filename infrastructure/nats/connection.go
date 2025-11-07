c47cecdecdfe1e0f38ce7e9831a9817cf284417e
Daniel322 <kravchenkodanil122@gmail.com>
2025-11-07 23:40:11 +0900

    Comeback

--- infrastructure/nats/connection.go
+++ infrastructure/nats/connection.go
@@ -1,7 +1,6 @@
 package nats_adapter
 
 import (
-	"fmt"
 	"log"
 
 	nats "github.com/nats-io/nats.go"
@@ -29,35 +28,35 @@ func NewNatsConnection(url string, options nats.Options) *NatsConnection {
 	}
 }
 
-func Connect(url string, options nats.Options) error {
-	_, err := nats.Connect(url, nats.UserInfo(options.User, options.Password), nats.PermissionErrOnSubscribe(true))
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println("succesfull connect to NATS!")
+// func Connect(url string, options nats.Options) error {
+// 	_, err := nats.Connect(url, nats.UserInfo(options.User, options.Password), nats.PermissionErrOnSubscribe(true))
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+// 	fmt.Println("succesfull connect to NATS!")
 
-	// natsConnection = nc
-	// fmt.Println(methods)
-	// _, err = natsConnection.Subscribe("$SRV.REGISTER", func(m *nats.Msg) {
-	// 	fmt.Println(m.Subject)
-	// 	var stringData string = string(m.Data)
-	// 	fmt.Println(m.Subject, stringData)
-	// 	var jsonData SrvRegisterMessage
-	// 	json.Unmarshal(m.Data, &jsonData)
-	// 	for _, v := range jsonData.Info.Endpoints {
-	// 		fmt.Println("v", v)
-	// 		if len(v.Metadata.Value) <= 0 && len(v.Metadata.ValueRegex) <= 0 {
-	// 			break
-	// 		}
-	// 	}
-	// 	fmt.Println("\n", jsonData)
-	// })
+// 	// natsConnection = nc
+// 	// fmt.Println(methods)
+// 	// _, err = natsConnection.Subscribe("$SRV.REGISTER", func(m *nats.Msg) {
+// 	// 	fmt.Println(m.Subject)
+// 	// 	var stringData string = string(m.Data)
+// 	// 	fmt.Println(m.Subject, stringData)
+// 	// 	var jsonData SrvRegisterMessage
+// 	// 	json.Unmarshal(m.Data, &jsonData)
+// 	// 	for _, v := range jsonData.Info.Endpoints {
+// 	// 		fmt.Println("v", v)
+// 	// 		if len(v.Metadata.Value) <= 0 && len(v.Metadata.ValueRegex) <= 0 {
+// 	// 			break
+// 	// 		}
+// 	// 	}
+// 	// 	fmt.Println("\n", jsonData)
+// 	// })
 
-	// if err != nil {
-	// 	fmt.Println("subscribe on srv info error", err)
-	// }
-	return err
-}
+// 	// if err != nil {
+// 	// 	fmt.Println("subscribe on srv info error", err)
+// 	// }
+// 	return err
+// }
 
 func (nats *NatsConnection) Disconnect() {
 	nats.Connection.Close()
